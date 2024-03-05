2df1dcb5efdab09c8cb4f6fd51e5821280552351
yofran010 <162249283+yofran010@users.noreply.github.com>
2024-03-04 19:24:05 -0800

    Update proyecto.php

--- proyecto.php
+++ proyecto.php
@@ -1,2 +1,2 @@
-osmar pendejo chupame el pene cabron culero de mierda sobame la riata zorra, me voy a cojer a su prima y la voy a preñar me vale verga todo
-atte:fri fayero
+osmar pendejo chupame el pene cabron culero de mierda sobame la riata zorra, me voy a cojer a tu prima y la voy a preñar me vale verga todo pendejo
+atte:harin
