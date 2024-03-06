c61fb92e731345e10e8d7754bb74ea154eb821fa
rmz09 <162284965+rmz09@users.noreply.github.com>
2024-03-05 18:45:21 -0800

    Update proyecto.php

--- proyecto.php
+++ proyecto.php
@@ -1,1 +1,31 @@
+<?php
 
+$articulo1 = [
+"producto" => "cereal"
+"gramos" => 70
+"precio" => 40
+];
+
+$articulo2 = [
+"producto" => "aceite"
+"mililitros" => 900
+"precio" => 25
+];
+
+$articulo3 = [
+"producto" => "arroz"
+"gramos" => 690
+"precio" => 20
+];
+
+$articulo4 = [
+"producto" => "pasta"
+"gramos" => 220
+"precio" => 15
+];
+
+$articulo5 = [
+"producto" => "sabritas"
+"gramos" => 42
+"precio" => 18
+];
