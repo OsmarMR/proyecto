d1c4b616567f88a0f3a359a4dbd7197f7baa053f
aiyoria123 <162249076+aiyoria123@users.noreply.github.com>
2024-04-09 19:17:56 -0700

    Update proyecto.php

--- proyecto.php
+++ proyecto.php
@@ -1,37 +1,37 @@
-require ("dotenv").config();
-const express = require ('express');
-const app = express();
-// Middleware de registro de peticiones a
-nivel raíz
-app.use ((req, res, next) →> ‹
-const method = req. method;
-const path = req. path;
-const ip = req. ip;
-console.10g ( '${method} ${path} - ${ip}
-*);
-next();
-}) ;
-}) ;
-module. exports = app;
+app.get('/json', (req, res) => {
+    var response = ""
+    console.log("MESSAGE_STYLE:" + process.env.MESSAGE_STYLE);
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = "HELLO JSON";
+    } else {
+      response = "Hello json";
+    }
+    res.send({"message": response})
+})
 
-app get('/json', (req, res) →> {
-let response = "";
+app.get('/now' , (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+    res.send({time: req.time})
+});
 
+app.get ("/:word/echo", (req, res) =>{
+  const word = req.params.word
+  res.json({echo:word})
+})
 
-console. 1og ("MESSAGE_STYLE:" +
+app.get("/name", (req, res) =>{
+  res.json({ name: req.query.first + " " + req.query.last })
+})
 
-process. env.MESSAGE_STYLE);
+app.post("/name", (req, res) => {
+  var string = req.body.first + " " + Request.body.last;
+  res.json({ name: string});
+});
 
-if (process. env.MESSAGE_STYLE ===
-"uppercase") {
-
-
-response = "HELLO JSON";
-} else {
-
-
-response = "Hello json";
-｝
-
-
-res. send ({"message": response});
+app.post("/name", function(req, res) {
+  // Handle the data in the request
+  var string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
+});
