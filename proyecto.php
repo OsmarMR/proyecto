57f1df7a05cd1bf6ff2a8e2f01431c127c7c900a
rmz09 <162284965+rmz09@users.noreply.github.com>
2024-04-09 17:49:16 -0700

    Update proyecto.php

--- proyecto.php
+++ proyecto.php
@@ -1,64 +1,20 @@
-let express = require('express');
+require("dotenv").config();
+let express = require('express')
 let app = express();
 
-
-
-console.log("Hello world")
-
-
-
-app.get("/", (req, res) => {
-  res.send('Hello Express')
+app.get('/json', (req, res) => {
+  var response = ""
+console.log("MESSAGE_STYLE:" + process.env.MESSAGE_STYLE);
+if (process.env.MESSAGE_STYLE === "uppercase") {
+  response = "HELLO JSON";
+} else {
+  response ="Hello json";
+}
+res.send({"message": response})
 })
 
-console.log("hello express")
 
 
 
 
-
-
-
-
-
-
-Opp-get(*/json', (red, 
-
-
-
-
-app-get ('/json', (reg, res)
-
-var response = *
-
-
-console. log ("MESSAGE_STYLE:" + process. env-MESSAGE_STYLE):
-
-if (process.env.MESSAGE_STYLE :
-"uppercase") {
-
-response = "HELLO JSON";
-} else (
-
-
-response = "Hello json";
-｝
-
-
-res. send (("message": response))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
+ module.exports = app;
