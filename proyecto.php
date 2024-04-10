f473840a9d4b93f6b35f5d43d605dacaf31d6f73
rmz09 <162284965+rmz09@users.noreply.github.com>
2024-04-09 18:08:20 -0700

    Actualizar proyecto.php

--- proyecto.php
+++ proyecto.php
@@ -1,20 +1,37 @@
-require("dotenv").config();
-let express = require('express')
-let app = express();
+require ("dotenv").config();
+const express = require ('express');
+const app = express();
+// Middleware de registro de peticiones a
+nivel raíz
+app.use ((req, res, next) →> ‹
+const method = req. method;
+const path = req. path;
+const ip = req. ip;
+console.10g ( '${method} ${path} - ${ip}
+*);
+next();
+}) ;
+}) ;
+module. exports = app;
 
-app.get('/json', (req, res) => {
-  var response = ""
-console.log("MESSAGE_STYLE:" + process.env.MESSAGE_STYLE);
-if (process.env.MESSAGE_STYLE === "uppercase") {
-  response = "HELLO JSON";
+app get('/json', (req, res) →> {
+let response = "";
+
+
+console. 1og ("MESSAGE_STYLE:" +
+
+process. env.MESSAGE_STYLE);
+
+if (process. env.MESSAGE_STYLE ===
+"uppercase") {
+
+
+response = "HELLO JSON";
 } else {
-  response ="Hello json";
-}
-res.send({"message": response})
-})
 
 
+response = "Hello json";
+｝
 
 
-
- module.exports = app;
+res. send ({"message": response});
