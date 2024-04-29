8035579516f7f77fa8fdaadc95946d4ab2d302a2
aiyoria123 <162249076+aiyoria123@users.noreply.github.com>
2024-04-29 15:41:12 -0700

    Update proyecto.php hola

--- proyecto.php
+++ proyecto.php
@@ -1,37 +1,73 @@
-app.get('/json', (req, res) => {
-    var response = ""
-    console.log("MESSAGE_STYLE:" + process.env.MESSAGE_STYLE);
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-    response = "HELLO JSON";
-    } else {
-      response = "Hello json";
-    }
-    res.send({"message": response})
-})
-
-app.get('/now' , (req, res, next) => {
-  req.time = new Date().toString();
-  next();
-}, (req, res) => {
-    res.send({time: req.time})
-});
-
-app.get ("/:word/echo", (req, res) =>{
-  const word = req.params.word
-  res.json({echo:word})
-})
-
-app.get("/name", (req, res) =>{
-  res.json({ name: req.query.first + " " + req.query.last })
-})
-
-app.post("/name", (req, res) => {
-  var string = req.body.first + " " + Request.body.last;
-  res.json({ name: string});
-});
-
-app.post("/name", function(req, res) {
-  // Handle the data in the request
-  var string = req.body.first + " " + req.body.last;
-  res.json({ name: string });
-});
+<HTML>
+<HEAD>
+<TITLE>JavaScript Index</TITLE>
+<script Language="JavaScript">
+function goback((
+alert('Good Byel"):
+history.go(-1);
+function gettheDate0 f
+Todays = new Date(;
+TheDate =""+ (Todays.getMonthO+
+1)+" / "+ Todays.getDate()+"/"+
+(Todays.getYear() +1900)
+document.clock.thedate.value =
+TheDate;
+var timerlD = null;
+var timerRunning= false;
+function stopclock (f
+if(timerRunning)
+clearTimeout(timerID):
+timerRunning= false;
+function startclock 0 (
+stopclock();
+gettheDate()
+showtime():
+function showtime 0
+var now = new Date();
+varhours = now.getHours();
+var minutes= now.getMinutes():
+var seconds = now.getSeconds(
+var timeValue= +((hours >12)
+hours -12 :hours)
+timeValue += (minutes < 10) ?
+:0": ":") + minutes
+timeValue+= ((seconds <10)?
+:0 : S + seconds
+timeValue += (hours >= 12) ? "
+P.M."
+A.M."
+document.clock.face.value
+timeValue;
+timerlD=
+setTimeout("showtime()",1000);
+timerRunning=true;
+</script>
+</HEAD>
+<BODY bgcolor="#00FFFF"
+onLoad="startclock()">
+<CENTER>
+<h2>Esto es un reloj hecho con
+JavaScript</h2>
+<table border>
+<tr
+<td><form name="clock"
+onSubmit=*0"></td>
+</tr>
+<tr>
+<td colspan=2>Hoy es: <input
+type="text" name="thedate" size=12
+value=""></td>
+<td colspan=2>La hora es: <input
+type="text" name="face" size=12
+value=""></td></form>
+<ftr>
+</table>
+</CENTER>
+<hr>
+<center>
+<h3>
+[<a href='javascr.htm">Volver</a>]
+</h3>
+</center>
+</BODY>
+</HTML>
