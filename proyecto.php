34d41bb38f574ec1b580f338f27920c8c350e196
aiyoria123 <162249076+aiyoria123@users.noreply.github.com>
2024-05-31 17:55:18 -0700

    Update proyecto.php

--- proyecto.php
+++ proyecto.php
@@ -1,69 +1,60 @@
-ja<html>
+</DOCTYPE=<html>
+<html>
+    <head>
+         <title>Ejemplo Switch</title>
+    </head>
+    <body>onload-"SeleccionarMes()">
+        <script type="text/javascript">
+        function SeleccionarMes() {
+        var mes- prompt8"Ingreso en el numero del mes deseado(1/12)
+        if(numeroMes !--null) {
+            var mes=parseInt(numeroMes);
 
- <head>
+        }
+        switch(mes){
+            Case 1:
+                 alert("Elegiste ENERO");
+                 break;
+            Case 2:
+                 alert("Elegiste FEBRERO");
+                 break;
+            Case 3:
+                 alert("Elegiste MARZO");
+                 break;
+            Case 4:
+                 alert("Elegiste ABRIL");
+                 break;
+            Case 5:
+                 alert("Elegiste MAYO");
+                 break;
+            Case 6:
+                 alert("Elegiste JUNIO");
+                 break;
+            Case 7:
+                 alert("Elegiste JULIO");
+                 break;
+            Case 8:
+                 alert("Elegiste AGOSTO");
+                 break;
+            Case 9:
+                 alert("Elegiste SEPTIEMBRE");
+                 break;
+            Case 10:
+                 alert("Elegiste OCTUBRE");
+                 break;
+            Case 11:
+                 alert("Elegiste NOVIEMBRE");
+                 break;
+            Case 12:
+                 alert("Elegiste DICIEMBRE");
+                 break;
 
-<meta charset="UTF-8">
-<title>Ciclo while</title>
-</head>
-<body bgcolor="#F781F3"><center>
-<marquee bgcolor=#890481 behavior=alternate><font color=#FFFFFF size=30 face="a Arbei Berry">Cecyte La Presa - Grupo 4EPGM</marquee>
-<h1><font face="a Area Stencil" size=15><b>Uso del ciclo While</font></h1>
-<h2>
-<script>
-var i=0;
-alert("Uso del ciclo while");
-document.write("Imprime los números del 0 al 10."+"</h2>"+"<br>");
-while (i<=10){
-document.write(i+"<br>");
- i++
- }
-</script>
-<br><br><br><br><br>
-<marquee bgcolor=#890481 direction=up scrolldelay=500><font color=#FFFFFF size=5 face="a Auto Signature"> <center>Mtra. Lourdes Ivette Torres Ibarra.</font></marquee>
-<center>
-</body>
+
+        }
+        }
+
+
+        </script>
+    </body>
 </html>
-
-
-
-
-<!
-
-
-
-
-Blert（"PlueoElgina con uso de 1a sentencia swittch "fin"topciones： "+"a"+"1. Opcion 1"+"n"t2. Opcionで＋1が"+3。Opcion 3）；
-var ni = parseint(prompt ("Selecciona una opción: "));
-document.write ("Numero: "+n1+"<br>") ;
-switch (n1) {
-case 1:
-document. write("<br>"+"Seleccionaste opción 1 "+"<br›");
-break
-case 2:
-document. write("<br>"+"Seleccionaste opción 2 "+"<br›");
-break
-case 3:
-document. write("<br›"+"Seleccionaste opción 3 "+"<br›");
-break default:
-document.write("‹br>"+"Esa opción no existe! ERROR 404... "+"<br>");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-DOCTYPE html><html lang="es"><head> <title>ejemplo ciclo for sencillo</title> <meta charset="utf-8"></head><body style="background-color: lightgreen;"><div align="center"><h2>Mostrar números con el ciclo FOR</h2></div><hr><b>Descripción: </b>mostrar los números deseados con el ciclo for<br><br><script>var i; for(i=2;i<=203;i=parseInt(i)+2) {document.write(i+" </body></html> ");}</script>
+</html>
